1eade4fa99412ccf52dc1a2f41f4d54d80378ae4
Pavan Pandya <pavanpandya016@gmail.com>
2025-12-31 15:28:41 +0530

    Add post admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Zeph
 
-# Register your models here.
+@admin.register(Zeph)
+class ZephAdmin(admin.ModelAdmin):
+    list_display = ("author", "content", "created_at", "updated_at")
+    list_filter = ("created_at", "updated_at")
+    search_fields = ("author__username", "content")
+    readonly_fields = ("created_at", "updated_at")
+    ordering = ("-created_at",) 
+
+    def short_content(self, obj):
+        return obj.content[:40] + "..." if len(obj.content) > 40 else obj.content
+
+    short_content.short_description = "Content"
