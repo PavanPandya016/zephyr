537059f0fd80b0bf93c9b45e547407bd05d014eb
Pavan Pandya <pavanpandya016@gmail.com>
2025-12-31 15:53:50 +0530

    Add post views

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Zeph
+
+class ZephListView(LoginRequiredMixin, ListView):
+    model = Zeph
+    template_name = "posts/feed.html"
+    context_object_name = "zephs"
+    paginate_by = 20
+
+class ZephDetailView(LoginRequiredMixin, DetailView):
+    model = Zeph
+    template_name = "posts/detail.html"
+
+class ZephCreatView(LoginRequiredMixin, CreateView):
+    model = Zeph
+    fields = ["content", "image", "parent"]
+    template_name = "posts/create.html"
+    success_url = reverse_lazy("post:feed")
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class ZephUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Zeph
+    fields = ["content", "image"]
+    template_name = "posts/edit.html"
+
+    def test_func(self):
+        return self.get_object().author == self.request.user
+    
+class ZephDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Zeph
+    template_name = "posts/delete.html"
+    success_url = reverse_lazy("post:feed")
+
+    def test_func(self):
+        return super().get_object().author == self.request.user
+    
