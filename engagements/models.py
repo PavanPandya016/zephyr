68bbc5df832372eb9539e29e453d49285e97eaaf
Pavan Pandya <pavanpandya016@gmail.com>
2026-01-01 22:07:04 +0530

    post

--- engagements/models.py
+++ engagements/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.conf import settings
+from posts.models import Zeph
 
-# Create your models here.
+class Like(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    zeph = models.ForeignKey(
+        Zeph,
+        on_delete=models.CASCADE,
+        related_name="likes",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "zeph")
+
+class Bookmark(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    zeph = models.ForeignKey(
+        Zeph,
+        on_delete=models.CASCADE, 
+        related_name="bookmarks"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "zeph")
